9c1ed69de6859ec3ff43921bb555b4651287fc8e
Gus657 <ggusperezg@gmail.com>
2020-04-28 19:55:56 -0600

    Entrevistas

--- src/routes/intreviews.js
+++ src/routes/intreviews.js
@@ -1,12 +1,38 @@
 const express  = require('express');
 const router  = express.Router();
 const passport = require('passport');
-const User = require('../models/user');
+const Intreview = require('../models/intreview');
+
+
+router.post('/intreviews',isAuthenticated,(req, res)=>{
+    const {body} = req;
+    return Intreview.create(body)
+    .then(createdIntreview=>{res.status(201).json(createdIntreview)})
+    .catch(err => {res.status(400).json(err)})
+});
 
 router.get('/intreviews',isAuthenticated,(req, res)=>{
-    res.send({"message":"Intreviews"})
-})
+    const {body} = req;
+    return Intreview.findOne({owner:body.owner})
+    .then(readedIntreview=>{res.status(200).json(readedIntreview)})
+    .catch(err => {res.status(400).json(err)})
+});
 
+router.patch('/intreviews',isAuthenticated,(req, res)=>{
+    const {body} = req;
+    return Intreview.findOneAndUpdate({code:body.code},body,{new:true,
+    useFindAndModify:false})
+    .then(modifiedIntreveiw=>{res.status(200).json(modifiedIntreveiw)})
+    .catch(err => {res.status(400).json(err)})
+});
+
+router.delete('/intreviews',isAuthenticated,(req, res)=>{
+    const {body} = req;
+    return Intreview.findOneAndDelete({code:body.code},{
+        useFindAndModify:false})
+    .then(deletedIntreview=>{res.status(200).json()})
+    .catch(err => {res.status(400).json(err)})
+});
 
 
 function isAuthenticated(req, res, next){
